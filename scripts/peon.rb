648a3b019a9ba06a66712f76d5d81c9d6e1821b4
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2018-07-25 13:12:04 -0300

    Add shared folder mapping

--- scripts/peon.rb
+++ scripts/peon.rb
@@ -10,5 +10,22 @@ class Peon
         end
       end
     end
+
+    if settings.include? 'shared_folders'
+      settings["shared_folders"].each do |folder|
+        if File.exists? File.expand_path(folder["src"])
+          config.vm.synced_folder folder["src"], folder["dst"], type: folder["type"] ||= nil
+
+          if Vagrant.has_plugin?("vagrant-bindfs")
+              config.bindfs.bind_folder folder["to"], folder["to"]
+          end
+        else
+          config.vm.provision "shell" do |s|
+            s.inline = ">&2 echo \"Unable to mount folder: #{folder['src']}\""
+          end
+        end        
+      end
+    end
+
   end
 end
