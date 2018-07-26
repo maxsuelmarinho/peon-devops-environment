99414b6203674cd2353bbbca9e17e4d720df5e0b
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2018-07-26 16:32:04 -0300

    Add instance configuration

--- Vagrantfile
+++ Vagrantfile
@@ -28,28 +28,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box_check_update = true
   config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
   config.ssh.forward_agent = true
-  config.vm.network :private_network, ip: "192.168.33.10"
-
-  config.vm.provider "virtualbox" do |vb|
-    required_plugins = %w(vagrant-vbguest vagrant-disksize)
-    required_plugins.each do |plugin|
-      unless Vagrant.has_plugin? plugin
-        system "vagrant plugin install #{plugin}"
-        _retry=true
-      end
-    end
-
-    config.vbguest.auto_update = true
-    config.disksize.size = "40GB"
-
-    vb.name = "peon-devops"
-    vb.linked_clone = true
-    vb.customize ["modifyvm", :id, "--memory", "1024"]
-    vb.customize ["modifyvm", :id, "--cpus", "1"]
-    vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-    vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
-  end
+  config.vm.network :private_network, ip: "192.168.33.10"  
 
   config.vm.provision "shell" do |s|
     s.name = "Install Ansible"
--- scripts/peon.rb
+++ scripts/peon.rb
@@ -1,6 +1,31 @@
 class Peon
   def Peon.work(config, settings)
 
+    config.vm.provider "virtualbox" do |vb|
+      required_plugins = %w(vagrant-vbguest vagrant-disksize)
+      required_plugins.each do |plugin|
+        unless Vagrant.has_plugin? plugin
+          system "vagrant plugin install #{plugin}"
+          _retry=true
+        end
+      end
+
+      config.vbguest.auto_update = true
+
+      if settings.include? 'instance_settings'
+        instanceSettings = settings["instance_settings"]
+        vb.customize ["modifyvm", :id, "--memory", instanceSettings["memory"] ||= "1024"]
+        vb.customize ["modifyvm", :id, "--cpus", instanceSettings["cpus"] ||= "1"]
+        config.disksize.size = instanceSettings["disk_size"] ||= "20GB"
+      end
+
+      vb.name = "peon-devops"
+      vb.linked_clone = true
+      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
+    end
+
     if settings.include? "ssh_private_keys"
       settings["ssh_private_keys"].each do |key|
         config.vm.provision "shell" do |s|
@@ -23,7 +48,7 @@ class Peon
           config.vm.provision "shell" do |s|
             s.inline = ">&2 echo \"Unable to mount folder: #{folder['src']}\""
           end
-        end        
+        end
       end
     end
 
