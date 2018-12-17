0a35269f3a0e425f8c7b4fb4d4ff25b58d16301a
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2018-12-16 23:57:25 -0200

    Add auto launching ssh agent script

---
+++ scripts/auto-launching-ssh-agent-config.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo "===================================="
+echo "Configuring auto launching ssh agent"
+echo "===================================="
+
+id_rsa_path="~/.ssh/id_rsa"
+
+if [ -z "$1" ]; then
+    id_rsa_path="$1"
+fi
+
+touch ~/.bashrc
+{
+    env=~/.ssh/agent.env
+
+    agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
+
+    agent_start () {
+        (umask 077; ssh-agent >| "$env")
+        . "$env" >| /dev/null ; }
+
+    agent_load_env
+
+    # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
+    agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+
+    if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+        agent_start
+        ssh-add $id_rsa_path
+    elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+        ssh-add $id_rsa_path
+    fi
+
+    unset env
+} >> ~/.bashrc
+
+cat $script_content >> ~/.bashrc
