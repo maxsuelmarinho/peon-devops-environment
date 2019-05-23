932f97478354b9e0af59ae3740ace07ebc4389d1
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-05-23 11:52:53 -0300

    Add flutter installation version

--- scripts/flutter-debian-install.sh
+++ scripts/flutter-debian-install.sh
@@ -4,12 +4,14 @@ echo "===================================="
 echo "Installing Flutter"
 echo "===================================="
 
+VERSION=1.2.1
+
 sudo apt-get update -y \
 && sudo apt-get install -y bash curl git unzip xz-utils
 
 cd /tmp \
-  && curl -LO https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.0.0-stable.tar.xz \
-  && tar -xvf flutter_linux_v1.0.0-stable.tar.xz \
+  && curl -LO https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v$VERSION-stable.tar.xz \
+  && tar -xvf flutter_linux_v$VERSION-stable.tar.xz \
   && mv -f flutter ~/ \
   && echo 'export PATH="$PATH:~/flutter/bin"' >> ~/.bashrc
 
